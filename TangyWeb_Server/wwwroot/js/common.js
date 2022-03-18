c682e14d89bdebbe2c3ebb3cf78f113d30466ab8
Mateus Palácio <mateuspalacio@gmail.com>
2022-03-18 17:28:29 -0300

    Lesson 59

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,25 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful", { timeOut: 5000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed", { timeOut: 5000 });
+    }
+}
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'success notificatiob!',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'success notificatiob!',
+            message,
+            'error'
+        )
+    }
+}
